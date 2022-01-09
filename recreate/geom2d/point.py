59e7c9e06afd07ffa0a9f1bfde49afde74493d5b
michael.mead <michaelmead007@icloud.com>
2022-01-09 15:52:00 -0600

    add geom2d

---
+++ recreate/geom2d/point.py
@@ -0,0 +1,19 @@
+import math
+from geom2d.vector import Vector
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def distance_to(self, other):
+        delta_x = other.x - self.x
+        delta_y = other.y - self.y
+        return math.sqrt(delta_x ** 2 + delta_y ** 2)
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
---
+++ recreate/geom2d/vector.py
@@ -0,0 +1,5 @@
+
+class Vector:
+    def __init__(self, u, v):
+        self.u = u
+        self.v = v
